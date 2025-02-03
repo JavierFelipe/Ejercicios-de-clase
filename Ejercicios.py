bcee7eaf94493d60e84e4fc8a823ba61b2dbe83e
JavierFelipe <jarodriguezac@unal.edu.co>
2025-02-03 00:07:44 -0500

    Ejercicios

---
+++ Ejercicios.py
@@ -0,0 +1,136 @@
+#1 Ejercicio#
+
+
+#L = ["Zidane", "Hierro", "Lampard", 12, "Ronaldo", "Roberto", 1]
+
+#m = 0
+
+#while m != 6:
+    
+    #r = L[m]
+
+    ####print(r, end = ", ")
+
+    #m += 1
+
+    #t = L[m:]
+
+    #s = list(t)
+
+    ####print(s, end = ", ")
+
+    #for x in s:
+        #if r == x:
+            #print("Sí hay elementos repetidos")
+        
+####################################################
+
+
+
+#2 ejercicio
+
+
+#N = ["radar", "oro", "Rafael", "Montaña"]
+#for palabra in N:
+    #t = list(palabra)
+    #r = t[::-1]
+    #if t == r:
+        #print("".join(t))
+        #break   
+#else:
+    #print("No existe")
+    
+####################################################
+
+
+
+#3 ejercicio
+
+#L = ["Mat", "sol", "ONU", "Bonn", "fl"]
+
+#v = "aeiouAEIOUáéíóúÁÉÍÓÚ"
+
+#c = len(L)
+
+
+#n = 0
+
+#while n < c:
+
+    #y = L[n]
+    #p = 0
+    #for letra in y:
+        #for t in v:
+            #while letra == t and p < 2:
+                #p += 1
+                #if p >= 2:
+                    #print("Esta palabra tiene una o más vocales: ", y)
+                #else:
+                    #print("No existe")
+                #break
+                
+    #n += 1
+
+#####################################################
+
+
+
+#4 ejercicio
+
+#l = [1, 5, 3, 1, 4, 2]
+
+#i = 0
+
+#n = len(l)
+
+#while i < n:
+
+    #p = l[i]
+
+    #r = l[n - 1 - i]
+
+    #i += 1
+
+    #if p == r:
+        #print("La cadena es palíndrome")
+        #break
+#else:
+    #print("La cadena no es palíndrome")
+        
+
+
+
+
+
+
+    
+                
+        
+                
+                
+
+
+
+
+
+
+
+
+
+
+
+        
+
+    
+ 
+
+
+
+
+    
+
+    
+    
+        
+    
+        
